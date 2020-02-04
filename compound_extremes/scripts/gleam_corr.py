4db6a2a1cc53454f1d87712c532792812974a7cd
Karen McKinnon <mckinnon@fas.harvard.edu>
2020-02-04 08:23:30 -0700

    added lags

--- compound_extremes/scripts/gleam_corr.py
+++ compound_extremes/scripts/gleam_corr.py
@@ -3,7 +3,11 @@ import xarray as xr
 import pandas as pd
 from glob import glob
 from compound_extremes import utils
-from time import time
+
+
+def demedian(x):
+    return (x - x.median())
+
 
 # ### Look at GLEAM soil moisture data
 #
@@ -15,7 +19,7 @@ from time import time
 # - calculate correlation of within-season variability
 
 sm_dir = '/glade/scratch/mckinnon/gleam'
-files = sorted(glob('%s/SMsurf*.nc' % sm_dir))
+files = sorted(glob('%s/SMsurf_*_GLEAM_v3.3a.nc' % sm_dir))
 savedir = '/glade/work/mckinnon/gsod/2506838728791974695/gleam'
 
 # Load all of our data
@@ -40,24 +44,22 @@ sm_lon_bnds = np.vstack((sm_lons - dlon/2, sm_lons + dlon/2))
 
 sm_years = 1980, 2018  # soil moisture data more limited
 
-
-def demedian(x):
-    return (x - x.median())
+# consider these lags between soil moisture and dew point
+lags = np.array([21, 14, 7, 0])
 
 
 appended_data = []
 for station_count, this_id in enumerate(station_ids):
     print('%i/%i' % (station_count, len(station_ids)))
-    t1 = time()
-    this_data = station_data[station_data['station_id'] == this_id]
+    this_data0 = station_data[station_data['station_id'] == this_id]
     this_lat = metadata.loc[metadata['station_id'] == this_id, 'lat']
     this_lon = metadata.loc[metadata['station_id'] == this_id, 'lon']
-    yrs = np.array([int(this_data['date'].values[ct].split('-')[0]) for ct in range(len(this_data))])
+    yrs = np.array([int(this_data0['date'].values[ct].split('-')[0]) for ct in range(len(this_data0))])
 
     # Pull out summer season
     summer_doy = station_stats.loc[station_stats['station_id'] == this_id, 'peakT']
-    doy1 = int(summer_doy.values[0].split(',')[0][1:])
-    doy2 = int(summer_doy.values[0].split(',')[-1][:-1])
+    doy1_dp = int(summer_doy.values[0].split(',')[0][1:])
+    doy2_dp = int(summer_doy.values[0].split(',')[-1][:-1])
 
     # Pull out soil moisture at closest gridbox
     # Note that, for islands, this may be nans
@@ -71,67 +73,81 @@ for station_count, this_id in enumerate(station_ids):
     seasonalSM, residualSM, SM_ann = utils.fit_seasonal_cycle(ds.time.dt.dayofyear.values,
                                                               ds.SMsurf[:, lon_idx, lat_idx].values)
 
-    sm_df = pd.DataFrame(data={'date': ds.time,
-                               'doy': ds.time.dt.dayofyear,
-                               'year': ds.time.dt.year,
-                               'SM_anoms': residualSM})
+    sm_df0 = pd.DataFrame(data={'date': ds.time,
+                                'doy': ds.time.dt.dayofyear,
+                                'year': ds.time.dt.year,
+                                'SM_anoms': residualSM})
 
     # Remove leap days
-    nonleap = sm_df['doy'] < 366
-    sm_df = sm_df.loc[nonleap, :]
+    nonleap = sm_df0['doy'] < 366
+    sm_df0 = sm_df0.loc[nonleap, :]
 
-    # Pull out summer season
-    if doy2 > doy1:  # boreal summer
-        idx_seasonal = (sm_df['doy'] >= doy1) & (sm_df['doy'] < doy2)
-    else:
-        idx_seasonal = (sm_df['doy'] >= doy1) | (sm_df['doy'] < doy2)
+    for lag in lags:
 
-    sm_df = sm_df.loc[idx_seasonal, :]
+        this_data = this_data0.copy()
+        sm_df = sm_df0.copy()
 
-    # Subselect dew point to soil moisture years
-    yrs = np.array([int(this_data['date'].values[ct].split('-')[0]) for ct in range(len(this_data))])
-    this_data = this_data.assign(year=yrs)
-    this_data = this_data[(this_data['year'] >= sm_years[0]) & (this_data['year'] <= sm_years[1])]
+        doy1 = doy1_dp - lag
+        doy2 = doy2_dp - lag
 
-    # For cases where summer season spans years, assign everything to the later year
-    if doy2 < doy1:  # austral summer
-        this_data.loc[this_data['doy'] <= doy2, 'year'] += 1
-        sm_df.loc[sm_df['doy'] <= doy2, 'year'] += 1
+        if doy1 < 0:
+            doy1 += 365
+        if doy2 < 0:
+            doy2 += 365
 
-    # Calculate various correlations
+        # Pull out summer season
+        if doy2 > doy1:  # boreal summer
+            idx_seasonal = (sm_df['doy'] >= doy1) & (sm_df['doy'] < doy2)
+        else:
+            idx_seasonal = (sm_df['doy'] >= doy1) | (sm_df['doy'] < doy2)
 
-    # Remove missing values
-    missing = np.isnan(this_data['dewp'].values)
-    sm_df = sm_df[~missing]
-    this_data = this_data[~missing]
+        sm_df = sm_df.loc[idx_seasonal, :]
 
-    this_data = this_data.assign(dewp_anoms=(this_data['dewp'].values - this_data['dewp_clim'].values))
+        # Subselect dew point to soil moisture years
+        yrs = np.array([int(this_data['date'].values[ct].split('-')[0]) for ct in range(len(this_data))])
+        this_data = this_data.assign(year=yrs)
+        this_data = this_data[(this_data['year'] >= sm_years[0]) & (this_data['year'] <= sm_years[1])]
 
-    # Raw
-    rho_raw = np.corrcoef(sm_df['SM_anoms'].values, this_data['dewp_anoms'].values)[0, 1]
+        # For cases where summer season spans years, assign everything to the later year
+        if doy2_dp < doy1_dp:
+            this_data.loc[this_data['doy'] <= doy2_dp, 'year'] += 1
+        if doy2 < doy1:  # austral summer
+            sm_df.loc[sm_df['doy'] <= doy2, 'year'] += 1
 
-    # Between seasonal medians
-    sm_ann = sm_df.groupby('year').median()
-    dewp_ann = this_data.groupby('year').median()
+        # Calculate various correlations
 
-    rho_ann = np.corrcoef(sm_ann['SM_anoms'].values, dewp_ann['dewp_anoms'].values)[0, 1]
+        # Remove missing values
+        missing = np.isnan(this_data['dewp'].values)
+        sm_df = sm_df[~missing]
+        this_data = this_data[~missing]
 
-    sm_anomaly = sm_df.groupby(sm_df.year).transform(demedian)
-    dewp_anomaly = this_data.groupby(this_data.year).transform(demedian)
+        this_data = this_data.assign(dewp_anoms=(this_data['dewp'].values - this_data['dewp_clim'].values))
 
-    rho_anoms = np.corrcoef(sm_anomaly['SM_anoms'].values, dewp_anomaly['dewp_anoms'].values)[0, 1]
+        # Raw
+        rho_raw = np.corrcoef(sm_df['SM_anoms'].values, this_data['dewp_anoms'].values)[0, 1]
 
-    # save
-    this_df = pd.DataFrame(data={'station_id': this_id,
-                                 'lat': this_lat,
-                                 'lon': this_lon,
-                                 'rho_raw': rho_raw,
-                                 'rho_ann': rho_ann,
-                                 'rho_anoms': rho_anoms})
+        # Between seasonal medians
+        sm_ann = sm_df.groupby('year').median()
+        dewp_ann = this_data.groupby('year').median()
 
-    appended_data.append(this_df)
-    dt = time() - t1
-    print('%02d seconds' % dt)
+        rho_ann = np.corrcoef(sm_ann['SM_anoms'].values, dewp_ann['dewp_anoms'].values)[0, 1]
+
+        sm_anomaly = sm_df.groupby(sm_df.year).transform(demedian)
+        dewp_anomaly = this_data.groupby(this_data.year).transform(demedian)
+
+        rho_anoms = np.corrcoef(sm_anomaly['SM_anoms'].values, dewp_anomaly['dewp_anoms'].values)[0, 1]
+
+        # save
+        this_df = pd.DataFrame(data={'station_id': this_id,
+                                     'lat': this_lat,
+                                     'lon': this_lon,
+                                     'lag': lag,
+                                     'rho_raw': rho_raw,
+                                     'rho_ann': rho_ann,
+                                     'rho_anoms': rho_anoms})
+
+        appended_data.append(this_df)
+
 appended_data = pd.concat(appended_data, ignore_index=True, sort=False)
 
-appended_data.to_csv('%s/gleam_dewp_corr.csv' % (savedir))
+appended_data.to_csv('%s/gleam_dewp_corr_lags.csv' % (savedir))
